cd39bdc69e34bf475e2aef4ea907bc7d2129b812
Neelam Choudhary <103179606+cneelam89@users.noreply.github.com>
2023-08-14 17:42:46 +0530

    Update views.py

--- Django-To-Do-list-with-user-authentication-master/base/views.py
+++ Django-To-Do-list-with-user-authentication-master/base/views.py
@@ -18,13 +18,7 @@ from .models import Task
 from .forms import PositionForm
 
 
-class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
-    fields = '__all__'
-    redirect_authenticated_user = True
 
-    def get_success_url(self):
-        return reverse_lazy('tasks')
 
 
 class RegisterPage(FormView):
